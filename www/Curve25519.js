a8445213f1e52a4ca6675bef02bccd01fccd6e3e
Ahmad Ben Mrad <batikhsouri@gmail.com>
2015-09-17 16:01:17 +0200

    Added JS plugin

---
+++ www/Curve25519.js
@@ -0,0 +1,27 @@
+"use strict";
+var exec = require('cordova/exec');
+
+function Curve25519(privateKey, publicKey, callback){
+	if (typeof callback != 'function') throw new TypeError('callback must be a function');
+	if (!(is_hex(privateKey) && privateKey.length == 64)){
+		callback('INVALID_PRIVATE_KEY');
+		return;
+	}
+	if (publicKey && !(is_hex(publicKey) && publicKey.length == 64)){
+		callback('INVALID_PUBLIC_KEY');
+		return;
+	}
+
+	var paramsArray = [privateKey];
+	if (publicKey) paramsArray.push(publicKey);
+
+	cordova.exec(resultHandler, callback, 'Curve25519', 'curve25519_donna', paramsArray);
+
+	function resultHandler(result){
+		callback(undefined, result);
+	}
+
+	function is_hex(s){
+		return typeof s == 'string' && s.length % 2 == 0 && /^([a-f]|[0-9])+$/ig.test(s);
+	}
+}
